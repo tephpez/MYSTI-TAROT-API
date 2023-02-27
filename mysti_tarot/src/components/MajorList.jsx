8dff6d54c5040cab04a53f4929380caec76244aa
Steph <tephpez@gmail.com>
2023-02-27 15:40:29 -0500

    styles

--- mysti_tarot/src/components/MajorList.jsx
+++ mysti_tarot/src/components/MajorList.jsx
@@ -9,25 +9,52 @@ console.log(props)
         navigate(`${index}`)
     }
 
+
+if (props && props.majCard){
     return(
        <div>
-
-        <h1>Hello</h1>
-         {/* <div className="grid">
+    
+        <h1>MAJOR ARCANA</h1>
+         <div className="cardContainer">
             {
-                props.results.map((major, index) => (
+                props.majCard.map((major, index) => (
                     <div key={index} className="card" onClick={() => showMajor(index)}>
-                        <h3>{major.name}</h3>
+                        <h3 className="cardName">{major.name}</h3>
+                        {/* <h4>{major.desc} </h4>
+                        <div id="upRevContainer">
+                <div id="upContainer">
+                    <span>UPRIGHT</span><br></br>
+                    <h3 className="resultsText"> {major.meaning_up} </h3>
+                </div>
+                <div id="revContainer">
+                    <span>REVERSED</span><br></br>
+                    <h3 className="resultsText"> {major.meaning_rev} </h3>
+                </div>
+            </div> */}
                     </div>
                 ))
             }
          </div>
-
+    
          <div>
              <Link to="/MinorLanding"><button>MINOR ARCANA</button></Link>
-         </div> */}
+         </div>
        </div> 
     )
+
+} else {
+    return  <h1> Loading Please Wait </h1>
+    
 }
+
+}
+
+
+// we have our cards mapped with all of our info 
+// we want to make it so when we select a card we click and we see all the info
+// map all cards with a background of a card.png put card name over it
+// when we select a card we'll see all of it's information pop up
+
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
